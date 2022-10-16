edc83eaefafee6e8f390e226c66d2581e7af7965
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-10-16 00:57:41 -0300

    limit

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,16 @@ import compression from 'compression';
 import helmet from 'helmet';
 import { PORT } from './config/env/server.constants';
 import mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // app.use(compression());
   app.use(helmet());
-
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT, () => {
     console.log(mongoose.STATES[mongoose.connection.readyState]);
     console.log(`server linten on Port: ${process.env.PORT}`);
