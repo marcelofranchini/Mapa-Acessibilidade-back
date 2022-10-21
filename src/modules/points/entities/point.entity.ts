e9905b30cef93ddfbfe713c4c0cdaef55a6359b4
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-10-21 14:44:57 -0300

    image optional

--- src/modules/points/entities/point.entity.ts
+++ src/modules/points/entities/point.entity.ts
@@ -11,7 +11,7 @@ export class Point {
   @Prop({ required: true })
   description: string;
 
-  @Prop({ required: true })
+  @Prop({ required: false })
   image: string;
 
   @Prop({ type: Object, required: true })
