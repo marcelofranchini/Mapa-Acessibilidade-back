ed120bc3c385d4c0fe20f4e82cf16a354c12ac74
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-10-22 16:29:01 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: 'https://mapaacessibilidadetcc.vercel.app',
+    origin: [
+      'https://mapaacessibilidadetcc.vercel.app',
+      'http://localhost:3030',
+    ],
     credentials: true,
   });
   // app.use(compression());
