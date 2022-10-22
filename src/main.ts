ed201ca33b42fec4c1a1635857ff5eb796cc8737
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-10-22 12:24:25 -0300

    up

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,16 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   // app.use(compression());
   app.use(helmet());
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(process.env.PORT || 3000 , () => {
+  await app.listen(process.env.PORT || 3000, () => {
     console.log(mongoose.STATES[mongoose.connection.readyState]);
     console.log(`server linten on Port: ${process.env.PORT}`);
   });
