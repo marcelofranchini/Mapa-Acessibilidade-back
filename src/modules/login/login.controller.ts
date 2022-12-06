eae5ba7ae06e756ceb0c5cb186f46eaa497e46be
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-12-06 11:30:41 -0300

    cors

--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -1,24 +1,12 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { loginDto } from './dto/login.dto';
 import { LoginService } from './login.service';
-import { Response } from 'express';
 
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
   @Post()
-  create(@Body() data: loginDto, @Res() res: Response) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-Requested-With,content-type,x-access-token,Authorization,access-x-btc,elegibility,vonageAuth,device-data,x-client-name',
-    );
-    res.setHeader('Access-Control-Allow-Credentials', true as any);
-
+  create(@Body() data: loginDto) {
     return this.loginService.login(data);
   }
 }
