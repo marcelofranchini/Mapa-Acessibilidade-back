38bb533a2d4134094859456b1706a074159252c3
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-12-06 11:09:41 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,25 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
 import * as bodyParser from 'body-parser';
+import { Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  app.use(function (req, res: Response, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type,x-access-token,Authorization,access-x-btc,elegibility,vonageAuth,device-data,x-client-name',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', true as any);
+    return next();
+  });
   // app.use(compression());
   app.use(helmet());
   app.use(bodyParser.json({ limit: '50mb' }));
--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { loginDto } from './dto/login.dto';
 import { LoginService } from './login.service';
 
@@ -6,7 +6,7 @@ import { LoginService } from './login.service';
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
   @Post()
-  create(@Body() data: loginDto) {
+  create(@Body() data: loginDto, @Res() res: Response) {
     return this.loginService.login(data);
   }
 }
